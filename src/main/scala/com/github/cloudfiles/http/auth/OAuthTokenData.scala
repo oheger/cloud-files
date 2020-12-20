8b436aed0bdb21d6ba4bec99204d12a683a92190
oheger <oliver.heger@oliver-heger.de>
2020-12-20 21:24:08 +0100

    Added a data class to represent OAuth tokens.
    The class contains both an access and a refresh token.
    
    The companion object has functionality to extract the tokens from a
    JSON token response.

---
+++ src/main/scala/com/github/cloudfiles/http/auth/OAuthTokenData.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.http.auth
+
+import java.util.regex.Pattern
+import scala.util.matching.Regex
+import scala.util.{Success, Try}
+
+object OAuthTokenData {
+  /** RegEx to extract the access token. */
+  private val regAccessToken = jsonPropRegEx("access_token")
+
+  /** RegEx to extract the refresh token. */
+  private val regRefreshToken = jsonPropRegEx("refresh_token")
+
+  /**
+   * Parses the JSON response of a token request and extracts the token values
+   * from it. The response must contain a property for the access token; the
+   * refresh token is optional - if it is missing, the passed in refresh token
+   * is used instead.
+   *
+   * @param json                the JSON response string to be parsed
+   * @param defaultRefreshToken the default refresh token
+   * @return a ''Try'' with the extracted ''OAuthTokenData''
+   */
+  def fromJson(json: String, defaultRefreshToken: String): Try[OAuthTokenData] = {
+    regAccessToken.findFirstMatchIn(json)
+      .map { accessToken =>
+        val refreshToken = regRefreshToken.findFirstMatchIn(json) map (_.group(1)) getOrElse defaultRefreshToken
+        OAuthTokenData(accessToken.group(1), refreshToken)
+      }.fold(Try[OAuthTokenData](throw new IllegalArgumentException("Could not parse token response: " +
+      json)))(Success(_))
+  }
+
+  /**
+   * Creates a regular expression that matches the value of the given JSON
+   * property.
+   *
+   * @param property the name of the property
+   * @return the regular expression for this property
+   */
+  private def jsonPropRegEx(property: String): Regex =
+    raw""""${Pattern.quote(property)}"\s*:\s*"([^"]+)"""".r
+}
+
+/**
+ * A data class representing the token material to be stored for a single
+ * OAuth client of a specific identity provider.
+ *
+ * @param accessToken  the access token
+ * @param refreshToken the refresh token
+ */
+case class OAuthTokenData(accessToken: String, refreshToken: String)
---
+++ src/test/scala/com/github/cloudfiles/http/auth/OAuthTokenDataSpec.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2020 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.http.auth
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.util.{Failure, Success}
+
+object OAuthTokenDataSpec {
+  /** Test access token. */
+  private val AccessToken = "thisIsTheAccessToken"
+
+  /** Test refresh token. */
+  private val RefreshToken = "theTokenToRefreshTheAccessToken"
+
+  /** The expected token data instance after parsing. */
+  private val ExpectedTokenData = OAuthTokenData(AccessToken, RefreshToken)
+
+  /** Prefix for the token response. */
+  private val JsonPrefix =
+    """
+      |{
+      |  "token_type": "bearer",
+      |  "expires_in": 3600,
+      |  "scope": "someScope"""".stripMargin
+
+  /**
+   * Appends a token value to a token response string.
+   *
+   * @param prefix    the prefix of the string
+   * @param tokenType the type of the token
+   * @param token     the token value
+   * @return the modified token result string
+   */
+  private def addToken(prefix: String, tokenType: String, token: String): String =
+    s"""$prefix,
+       |  "$tokenType": "$token"""".stripMargin
+}
+
+/**
+ * Test class for ''OAuthTokenData''.
+ */
+class OAuthTokenDataSpec extends AnyFlatSpec with Matchers {
+
+  import OAuthTokenDataSpec._
+
+  "OAuthTokenData" should "parse a valid JSON response with both tokens" in {
+    val TokenResponse = addToken(addToken(JsonPrefix, "access_token", AccessToken),
+      "refresh_token", RefreshToken) + "}"
+
+    OAuthTokenData.fromJson(TokenResponse, "") match {
+      case Failure(exception) =>
+        fail("Could not parse response", exception)
+      case Success(value) =>
+        value should be(ExpectedTokenData)
+    }
+  }
+
+  it should "parse a valid JSON response with only the access token" in {
+    val TokenResponse = addToken(JsonPrefix, "access_token", AccessToken) + "}"
+
+    OAuthTokenData.fromJson(TokenResponse, RefreshToken) match {
+      case Failure(exception) =>
+        fail("Could not parse response", exception)
+      case Success(value) =>
+        value should be(ExpectedTokenData)
+    }
+  }
+
+  it should "parse a JSON response without an access token" in {
+    val TokenResponse = addToken(JsonPrefix, "refresh_token", RefreshToken) + "}"
+
+    OAuthTokenData.fromJson(TokenResponse, "*") match {
+      case Failure(exception) =>
+        exception shouldBe a[IllegalArgumentException]
+        exception.getMessage should include(TokenResponse)
+      case r => fail("Unexpected result: " + r)
+    }
+  }
+}

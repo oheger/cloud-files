ab1ce5f5bc002ad28719a3029d6feab58ddc5dbc
oheger <oliver.heger@oliver-heger.de>
2021-02-16 21:47:50 +0100

    Added a flow stage to encrypt data while flowing through a stream.
    This stage can be added to a stream of type ByteString. It applies the
    transformation controlled by a CryptCipher to the data that passes the
    stage.
    
    This makes it possible to easily add encryption or decryption to use
    cases based on streaming.

---
+++ crypt/src/main/scala/com/github/cloudfiles/crypt/service/CryptStage.scala
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.crypt.service
+
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import akka.util.ByteString
+import com.github.cloudfiles.crypt.alg.CryptCipher
+
+import java.security.SecureRandom
+
+/**
+ * A flow stage to transform data using a [[CryptCipher]].
+ *
+ * This class supports the transformation of data via a ''CryptCipher'' while
+ * it flows through a stream. The protocol of the cipher is invoked to make
+ * sure that the chunks of data are transformed correctly. That way even
+ * large sets of data can be transformed efficiently.
+ *
+ * @param cryptCipher the object to transform the data in the stream
+ * @param random      the source for randomness
+ */
+class CryptStage(val cryptCipher: CryptCipher)(implicit random: SecureRandom)
+  extends GraphStage[FlowShape[ByteString, ByteString]] {
+
+  /**
+   * Definition of a processing function. The function expects a block of data
+   * and a cipher and produces a block of data to be passed downstream.
+   */
+  type CryptFunc = (ByteString, CryptCipher) => ByteString
+
+  val in: Inlet[ByteString] = Inlet[ByteString]("CryptStage.in")
+  val out: Outlet[ByteString] = Outlet[ByteString]("CryptStage.out")
+
+  override val shape: FlowShape[ByteString, ByteString] = FlowShape.of(in, out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+      /** The current processing function. */
+      private var processingFunc: CryptFunc = initProcessing
+
+      /**
+       * A flag whether data has been received by this stage. This is used to
+       * determine whether a final block of data has to be handled when
+       * upstream ends.
+       */
+      private var dataProcessed = false
+
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          val data = grab(in)
+          val processedData = processingFunc(data, cryptCipher)
+          push(out, processedData)
+        }
+
+        override def onUpstreamFinish(): Unit = {
+          if (dataProcessed) {
+            val finalBytes = cryptCipher.complete()
+            if (finalBytes.nonEmpty) {
+              push(out, ByteString(finalBytes))
+            }
+          }
+          super.onUpstreamFinish()
+        }
+      })
+
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          pull(in)
+        }
+      })
+
+      /**
+       * Handles initialization of data processing. This is the ''CryptFunc''
+       * that is called for the first block of data received. It initializes
+       * the managed cipher object, pushes the first data block downstream,
+       * and sets the actual processing function.
+       *
+       * @param data   the first chunk of data
+       * @param cipher the cipher object
+       * @return the data to be pushed downstream
+       */
+      private def initProcessing(data: ByteString, cipher: CryptCipher): ByteString = {
+        dataProcessed = true
+        val result = cipher.init(random, data)
+        processingFunc = cryptFunc
+        result
+      }
+    }
+
+  /**
+   * Returns the function executing encryption / decryption logic. This
+   * function is called when initialization is done to process further data
+   * chunks.
+   *
+   * @return the function for processing data chunks
+   */
+  private def cryptFunc: CryptFunc = (chunk, cipher) =>
+    cipher.transform(chunk)
+}
---
+++ crypt/src/test/scala/com/github/cloudfiles/crypt/service/CryptStageSpec.scala
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.crypt.service
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.stream.FlowShape
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.stage.GraphStage
+import akka.util.ByteString
+import com.github.cloudfiles.core.{AsyncTestHelper, FileTestHelper}
+import com.github.cloudfiles.crypt.alg.ShiftCryptAlgorithm
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import java.security.SecureRandom
+
+object CryptStageSpec {
+
+  /** Group size for splitting text input for encryption. */
+  private val GroupSize = 64
+
+  /** A secure random object in implicit scope. */
+  private implicit val random: SecureRandom = new SecureRandom
+
+  /**
+   * Splits a text into a number of byte string blocks with a standard chunk
+   * size.
+   *
+   * @param text the input text
+   * @return the resulting blocks
+   */
+  private def splitPlainText(text: String): List[ByteString] =
+    text.grouped(GroupSize).map(s => ByteString(s)).toList
+
+  /**
+   * Combines a list of binary chunks and returns the result as string.
+   *
+   * @param data the list of data chunks
+   * @return the resulting string
+   */
+  private def combine(data: List[ByteString]): String =
+    data.foldLeft(ByteString.empty)((buf, s) => buf ++ s).utf8String
+}
+
+class CryptStageSpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers with AsyncTestHelper {
+
+  import CryptStageSpec._
+
+  /**
+   * Runs a stream that encrypts or decrypts the given data.
+   *
+   * @param data       the data to be processed
+   * @param cryptStage the stage for encryption / decryption
+   * @return the list with resulting data chunks
+   */
+  private def runCryptStream(data: List[ByteString], cryptStage: GraphStage[FlowShape[ByteString, ByteString]]):
+  List[ByteString] = {
+    val source = Source(data)
+    val sink = Sink.fold[List[ByteString], ByteString](Nil)((list, bs) => bs :: list)
+    val futResult = source.via(cryptStage)
+      .runWith(sink)
+    futureResult(futResult).reverse
+  }
+
+  /**
+   * Runs a stream to encrypt the given message. The message is split into
+   * chunks before.
+   *
+   * @param message the message to be encrypted
+   * @return the resulting encrypted chunks
+   */
+  private def encrypt(message: String): List[ByteString] =
+    runCryptStream(splitPlainText(message),
+      new CryptStage(ShiftCryptAlgorithm.encryptCipher(ShiftCryptAlgorithm.encryptKey)))
+
+  /**
+   * Runs a stream that decrypts the given data chunks and returns the result
+   * as string.
+   *
+   * @param cipherText the chunks of the encrypted message
+   * @return the resulting decrypted text
+   */
+  private def decrypt(cipherText: List[ByteString]): String =
+    combine(runCryptStream(cipherText,
+      new CryptStage(ShiftCryptAlgorithm.decryptCipher(ShiftCryptAlgorithm.decryptKey))))
+
+  /**
+   * Checks an encryption followed by a decryption. This should result in the
+   * original message.
+   *
+   * @param message the message to be processed
+   */
+  private def checkRoundTrip(message: String): Unit = {
+    val encrypted = encrypt(message)
+    val processed = decrypt(encrypted)
+    processed should be(message)
+  }
+
+  "A CryptStage" should "produce the same text when encrypting and decrypting" in {
+    checkRoundTrip(FileTestHelper.TestData)
+  }
+
+  it should "handle an empty source to encrypt" in {
+    val stage = new CryptStage(ShiftCryptAlgorithm.encryptCipher(ShiftCryptAlgorithm.encryptKey))
+
+    runCryptStream(Nil, stage) should have size 0
+  }
+
+  it should "handle an empty source to decrypt" in {
+    decrypt(Nil) should be("")
+  }
+
+  it should "produce encrypted text" in {
+    val cipherText = combine(encrypt(FileTestHelper.TestData))
+
+    cipherText should be(ShiftCryptAlgorithm.encrypt(ByteString(FileTestHelper.TestData)).utf8String)
+  }
+
+  it should "handle small messages as well" in {
+    val Message = "Test"
+
+    checkRoundTrip(Message)
+  }
+}

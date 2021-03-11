37a290d8816def784c52ff6ccbd6ee2facaca52e
oheger <oliver.heger@oliver-heger.de>
2021-03-11 22:06:06 +0100

    Added an implementation of the HttpRequestSenderFactory trait.
    The HttpRequestSenderFactoryImpl module implements the logic to create
    and configure a request sender actor based on a configuration. A test
    class checks all options.

---
+++ core/src/main/scala/com/github/cloudfiles/core/http/factory/HttpRequestSenderFactoryImpl.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.core.http.factory
+
+import akka.actor.typed.ActorRef
+import akka.http.scaladsl.model.Uri
+import com.github.cloudfiles.core.http.{HttpRequestSender, RetryAfterExtension}
+import com.github.cloudfiles.core.http.auth.{BasicAuthConfig, BasicAuthExtension, NoAuthConfig, OAuthConfig, OAuthExtension}
+
+/**
+ * A default implementation of [[HttpRequestSenderFactory]].
+ */
+object HttpRequestSenderFactoryImpl extends HttpRequestSenderFactory {
+  /** Suffix for the name generated for a Basic Auth extension. */
+  final val BasicAuthName = "_BasicAuth"
+
+  /** Suffix for the name generated for an OAuth extension. */
+  final val OAuthName = "_OAuth"
+
+  /** Suffix for the name generated for the actor for IDP communication. */
+  final val IDPName = "_IDP"
+
+  /** Suffix for the name generated for a RetryAfter extension. */
+  final val RetryAfterName = "_Retry"
+
+  override def createRequestSender(spawner: Spawner, baseUri: Uri, config: HttpRequestSenderConfig):
+  ActorRef[HttpRequestSender.HttpCommand] =
+    decorateRequestSender(spawner, spawner.spawn(HttpRequestSender(baseUri, config.queueSize), config.actorName),
+      config)
+
+  override def decorateRequestSender(spawner: Spawner, requestSender: ActorRef[HttpRequestSender.HttpCommand],
+                                     config: HttpRequestSenderConfig): ActorRef[HttpRequestSender.HttpCommand] = {
+    val authActor = config.authConfig match {
+      case basicAuth: BasicAuthConfig =>
+        spawner.spawn(BasicAuthExtension(requestSender, basicAuth), actorName(config, BasicAuthName))
+      case oauthConfig: OAuthConfig =>
+        val idpActor = spawner.spawn(HttpRequestSender(oauthConfig.tokenEndpoint, config.queueSize),
+          actorName(config, IDPName))
+        spawner.spawn(OAuthExtension(requestSender, idpActor, oauthConfig), actorName(config, OAuthName))
+      case NoAuthConfig => requestSender
+    }
+
+    config.retryAfterConfig.fold(authActor) { retryConfig =>
+      spawner.spawn(RetryAfterExtension(authActor, retryConfig), actorName(config, RetryAfterName))
+    }
+  }
+
+  /**
+   * Convenience function to generate an optional actor name derived from the
+   * base actor name as specified in the configuration.
+   *
+   * @param config the ''HttpRequestSenderConfig''
+   * @param suffix the suffix to append to the base actor name
+   * @return the resulting optional actor name
+   */
+  private def actorName(config: HttpRequestSenderConfig, suffix: String): Option[String] =
+    config.actorName map (_ + suffix)
+}
---
+++ core/src/test/scala/com/github/cloudfiles/core/http/factory/HttpRequestSenderFactoryImplITSpec.scala
@@ -0,0 +1,198 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.core.http.factory
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.{ActorRef, Behavior, Props}
+import akka.http.scaladsl.model.{HttpRequest, StatusCodes}
+import akka.util.Timeout
+import com.github.cloudfiles.core.WireMockSupport.{BasicAuthFunc, TokenAuthFunc}
+import com.github.cloudfiles.core.http.RetryAfterExtension.RetryAfterConfig
+import com.github.cloudfiles.core.http.auth.{BasicAuthConfig, OAuthConfig, OAuthTokenData}
+import com.github.cloudfiles.core.http.{HttpRequestSender, Secret}
+import com.github.cloudfiles.core.{AsyncTestHelper, FileTestHelper, WireMockSupport}
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, get, stubFor, urlPathEqualTo}
+import com.github.tomakehurst.wiremock.stubbing.Scenario
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration._
+
+object HttpRequestSenderFactoryImplITSpec {
+  /** The path for sending test requests. */
+  private val Path = "/request"
+
+  /** The name of the file with the response of the test request. */
+  private val ResponseFile = "response.txt"
+
+  /** A timeout for querying the actor under test. */
+  private implicit val RequestTimeout: Timeout = Timeout(3.seconds)
+}
+
+/**
+ * Test class for ''HttpRequestSenderFactoryImpl''.
+ */
+class HttpRequestSenderFactoryImplITSpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers
+  with WireMockSupport with AsyncTestHelper {
+  override protected val resourceRoot: String = "core"
+
+  import HttpRequestSenderFactoryImplITSpec._
+
+  /**
+   * Returns a ''Spawner'' that uses the actor testkit to spawn new actors.
+   * Optionally, a map can be provided, in which newly created named actors are
+   * stored.
+   *
+   * @param namedActors a map where to store named actors
+   * @return the ''Spawner'' for this test class
+   */
+  private def testSpawner(namedActors: collection.mutable.Map[String, ActorRef[_]] = collection.mutable.Map.empty):
+  Spawner = new Spawner {
+    override def spawn[T](behavior: Behavior[T], optName: Option[String], props: Props): ActorRef[T] = {
+      props should be(Props.empty)
+      optName match {
+        case Some(name) =>
+          val ref = testKit.spawn(behavior, name)
+          namedActors += (name -> ref)
+          ref
+        case None =>
+          testKit.spawn(behavior)
+      }
+    }
+  }
+
+  /**
+   * Checks request sending via a test actor based on the passed in
+   * configuration. Sends a test request and checks whether the expected
+   * response is received.
+   *
+   * @param config the configuration for the test actor
+   */
+  private def checkRequestSending(config: HttpRequestSenderConfig): Unit = {
+    val actor = HttpRequestSenderFactoryImpl.createRequestSender(testSpawner(), serverBaseUri, config)
+    val probe = testKit.createTestProbe[HttpRequestSender.Result]()
+    val request = HttpRequestSender.SendRequest(HttpRequest(uri = Path), "testRequestData", probe.ref)
+    actor ! request
+    val result = probe.expectMessageType[HttpRequestSender.SuccessResult]
+    result.response.status should be(StatusCodes.Accepted)
+    val content = futureResult(entityToString(result.response))
+    content should be(FileTestHelper.TestDataSingleLine)
+  }
+
+  "HttpRequestSenderFactoryImpl" should "create a basic HTTP sender actor" in {
+    stubFor(get(urlPathEqualTo(Path))
+      .willReturn(aResponse()
+        .withStatus(StatusCodes.Accepted.intValue)
+        .withBodyFile(ResponseFile)))
+
+    checkRequestSending(HttpRequestSenderConfig())
+  }
+
+  it should "create a basic HTTP sender actor with a specific name" in {
+    val ActorName = "basicHttpSenderActor"
+    val config = HttpRequestSenderConfig(actorName = Some(ActorName))
+
+    val sender = HttpRequestSenderFactoryImpl.createRequestSender(testSpawner(), serverBaseUri, config)
+    sender.path.toString should endWith(ActorName)
+  }
+
+  it should "create an HTTP sender actor with support for Basic Auth" in {
+    stubFor(BasicAuthFunc(get(urlPathEqualTo(Path)))
+      .willReturn(aResponse()
+        .withStatus(StatusCodes.Accepted.intValue)
+        .withBodyFile(ResponseFile)))
+    val authConfig = BasicAuthConfig(WireMockSupport.UserId, Secret(WireMockSupport.Password))
+    val config = HttpRequestSenderConfig(authConfig = authConfig)
+
+    checkRequestSending(config)
+  }
+
+  it should "derive a name for the Basic Auth actor" in {
+    val BaseName = "httpSenderWithBasicAuth"
+    val namedActors = collection.mutable.Map.empty[String, ActorRef[_]]
+    val spawner = testSpawner(namedActors)
+    val baseActor = HttpRequestSenderFactoryImpl.createRequestSender(spawner, serverBaseUri,
+      HttpRequestSenderConfig())
+    namedActors should have size 0
+
+    val authConfig = BasicAuthConfig(WireMockSupport.UserId, Secret(WireMockSupport.Password))
+    val config = HttpRequestSenderConfig(authConfig = authConfig, actorName = Some(BaseName))
+    HttpRequestSenderFactoryImpl.decorateRequestSender(spawner, baseActor, config)
+    namedActors.keys should contain only (BaseName + HttpRequestSenderFactoryImpl.BasicAuthName)
+  }
+
+  it should "create an HTTP sender actor with support for OAuth tokens" in {
+    val Token = "MyAccessToken"
+    stubFor(TokenAuthFunc(Token)(get(urlPathEqualTo(Path))
+      .willReturn(aResponse()
+        .withStatus(StatusCodes.Accepted.intValue)
+        .withBodyFile(ResponseFile))))
+    val authConfig = OAuthConfig(serverUri("/token"), "someRedirect", "someClient",
+      Secret("someSecret"), OAuthTokenData(Token, "someRefreshToken"))
+    val config = HttpRequestSenderConfig(authConfig = authConfig)
+
+    checkRequestSending(config)
+  }
+
+  it should "construct a correct request sender for the IDP to support token refresh operations" in {
+    val TokenPath = "/token"
+    val BaseName = "httpSenderWithOAuth"
+    stubFor(get(urlPathEqualTo(TokenPath))
+      .willReturn(aResponse().withStatus(StatusCodes.OK.intValue)))
+    val authConfig = OAuthConfig(serverUri(TokenPath), "someRedirect", "someClient",
+      Secret("someSecret"), OAuthTokenData("someAccessToken", "someRefreshToken"))
+    val namedActors = collection.mutable.Map.empty[String, ActorRef[_]]
+    val spawner = testSpawner(namedActors)
+    val config = HttpRequestSenderConfig(authConfig = authConfig, actorName = Some(BaseName))
+
+    HttpRequestSenderFactoryImpl.createRequestSender(spawner, "https://test.example.org", config)
+    namedActors.keys should contain only(BaseName, BaseName + HttpRequestSenderFactoryImpl.OAuthName,
+      BaseName + HttpRequestSenderFactoryImpl.IDPName)
+    val idpSender = namedActors(BaseName + HttpRequestSenderFactoryImpl.IDPName)
+      .asInstanceOf[ActorRef[HttpRequestSender.HttpCommand]]
+    val result = futureResult(HttpRequestSender.sendRequestSuccess(idpSender, HttpRequest(uri = TokenPath), this))
+    result.response.status should be(StatusCodes.OK)
+  }
+
+  it should "create an HTTP sender actor with support for retrying requests" in {
+    val ScenarioName = "Retry"
+    val StateRetry = "ExpectRetry"
+    stubFor(get(urlPathEqualTo(Path)).inScenario(ScenarioName)
+      .whenScenarioStateIs(Scenario.STARTED)
+      .willReturn(aResponse().withStatus(StatusCodes.TooManyRequests.intValue))
+      .willSetStateTo(StateRetry))
+    stubFor(get(urlPathEqualTo(Path)).inScenario(ScenarioName)
+      .whenScenarioStateIs(StateRetry)
+      .willReturn(aResponse()
+        .withStatus(StatusCodes.Accepted.intValue)
+        .withBodyFile(ResponseFile)))
+    val config = HttpRequestSenderConfig(retryAfterConfig = Some(RetryAfterConfig()))
+
+    checkRequestSending(config)
+  }
+
+  it should "derive a name for the retry after extension" in {
+    val BaseName = "httpSenderWithRetry"
+    val namedActors = collection.mutable.Map.empty[String, ActorRef[_]]
+    val spawner = testSpawner(namedActors)
+    val config = HttpRequestSenderConfig(actorName = Some(BaseName),
+      retryAfterConfig = Some(RetryAfterConfig()))
+
+    HttpRequestSenderFactoryImpl.createRequestSender(spawner, "https://test.example.org", config)
+    namedActors.keys should contain only(BaseName, BaseName + HttpRequestSenderFactoryImpl.RetryAfterName)
+  }
+}

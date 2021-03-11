6d6b3d804ff49648195b23075897941733562fbb
oheger <oliver.heger@oliver-heger.de>
2021-03-11 22:04:20 +0100

    Added a trait for creating configured request sender actors.
    The idea is to have an HttpRequestSenderConfig class that defines the
    properties of the new sender actor. This is passed to the factory,
    which creates the new actor instance and decorates it with the
    extensions desired.
    
    This simplifies the creation of initialized request sender actors and
    also makes it easier to test for client code.

---
+++ core/src/main/scala/com/github/cloudfiles/core/http/factory/HttpRequestSenderConfig.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.core.http.factory
+
+import com.github.cloudfiles.core.http.HttpRequestSender
+import com.github.cloudfiles.core.http.RetryAfterExtension.RetryAfterConfig
+import com.github.cloudfiles.core.http.auth.{AuthConfig, NoAuthConfig}
+
+/**
+ * A configuration class describing desired features of an actor for sending
+ * HTTP requests.
+ *
+ * An instance of this class is expected by [[HttpRequestSenderFactory]]. It
+ * describes the features the resulting actor must support. The properties all
+ * have default values, so client code only needs to configure the properties
+ * it wants to change.
+ *
+ * @param actorName        an optional actor name; if defined, the resulting
+ *                         actor is given this name (if multiple actors are
+ *                         created for other features, their names are derived
+ *                         from this base name); otherwise, anonymous actors
+ *                         are created
+ * @param authConfig       the configuration for the authentication mechanism
+ *                         to be used
+ * @param queueSize        the size of the request queue
+ * @param retryAfterConfig an optional configuration for a ''RetryAfter''
+ *                         extension; if present, such an extension is created
+ */
+case class HttpRequestSenderConfig(actorName: Option[String] = None,
+                                   authConfig: AuthConfig = NoAuthConfig,
+                                   queueSize: Int = HttpRequestSender.DefaultQueueSize,
+                                   retryAfterConfig: Option[RetryAfterConfig] = None)
---
+++ core/src/main/scala/com/github/cloudfiles/core/http/factory/HttpRequestSenderFactory.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.core.http.factory
+
+import akka.actor.typed.ActorRef
+import akka.http.scaladsl.model.Uri
+import com.github.cloudfiles.core.http.HttpRequestSender
+
+/**
+ * A trait to simplify the creation and configuration of actors for sending
+ * HTTP requests.
+ *
+ * The ''http'' package provides several extension actors to modify the
+ * behavior of a plain [[HttpRequestSender]] actor, including support for
+ * multiple authentication schemes. Usage of these features requires the
+ * creation of multiple actors, which are then plugged together.
+ *
+ * This trait supports a more declarative way of constructing HTTP request
+ * sender actors. The idea is to define the desired features of the resulting
+ * actor using an [[HttpRequestSenderConfig]] object. A concrete implementation
+ * of this trait must evaluate the configuration and construct an actor that
+ * supports all the features requested. This not only simplifies client code
+ * but also increases testability, as mock implementations of this trait can be
+ * used.
+ */
+trait HttpRequestSenderFactory {
+  /**
+   * Creates a new actor instance for sending HTTP requests to the specified
+   * URI that satisfies the criteria defined by the given configuration. Note
+   * that the resulting actor is fully initialized; it is not necessary to call
+   * ''decorateRequestSender()'' manually.
+   *
+   * @param spawner the object to create new actor instances
+   * @param baseUri the URI of the host supported by the new actor
+   * @param config  the configuration of the request sender actor
+   * @return a reference to the newly created actor
+   */
+  def createRequestSender(spawner: Spawner, baseUri: Uri, config: HttpRequestSenderConfig):
+  ActorRef[HttpRequestSender.HttpCommand]
+
+  /**
+   * Decorates the given actor for sending HTTP requests to support all the
+   * features declared by the passed in configuration. Adding features to the
+   * given actor typically requires the creation of new actors that wrap the
+   * original one. The function returns an ''ActorRef'' that has been
+   * configured with all desired properties. This function can be used by
+   * clients that already have a basic HTTP sender actor and want to enhance
+   * it.
+   *
+   * @param spawner       the object to create new actor instances
+   * @param requestSender the original request sender actor to be extended
+   * @param config        the configuration of the request sender actor
+   * @return a reference to the decorated actor
+   */
+  def decorateRequestSender(spawner: Spawner, requestSender: ActorRef[HttpRequestSender.HttpCommand],
+                            config: HttpRequestSenderConfig): ActorRef[HttpRequestSender.HttpCommand]
+}

94a59f8a4e313bcfc0b814e4df2c5a2b623d55c3
oheger <oliver.heger@oliver-heger.de>
2021-04-18 21:23:18 +0200

    Added a basic implementation of the PathResolver trait.
    PathComponentsResolver implements the resolving logic that has been
    used by CryptNamesFileSystem so far. It resolves the single path
    components one by one without making use of caching or other
    optimizations.

---
+++ crypt/src/main/scala/com/github/cloudfiles/crypt/fs/resolver/PathComponentsResolver.scala
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.crypt.fs.resolver
+
+import akka.actor.typed.ActorSystem
+import com.github.cloudfiles.core.FileSystem.Operation
+import com.github.cloudfiles.core.{FileSystem, Model}
+import com.github.cloudfiles.crypt.fs.CryptConfig
+import com.github.cloudfiles.crypt.service.CryptService
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+ * A specialized ''PathResolver'' implementation that resolves paths directly
+ * by matching the single components against the decrypted path names on each
+ * level.
+ *
+ * This is the most basic resolver to be used if the single path components
+ * cannot simply be encrypted (because encryption involves a random initial
+ * vector, so that each encrypt operation yields different results). The
+ * implementation navigates through the single path components. For each
+ * component, the folder content is requested, and the names of its elements
+ * are decrypted, until a match is found. This is a rather expensive operation.
+ *
+ * This implementation is appropriate if only few resolve operations are done,
+ * and the client mainly uses IDs to access elements directly. In this case,
+ * for the few resolve operations there is no benefit in spending additional
+ * effort in caching or other optimizations.
+ *
+ * @tparam ID     the type of IDs used by the file system
+ * @tparam FILE   the type of files
+ * @tparam FOLDER the type of folders
+ */
+class PathComponentsResolver[ID, FILE <: Model.File[ID], FOLDER <: Model.Folder[ID]]
+  extends PathResolver[ID, FILE, FOLDER] {
+  override def resolve(components: Seq[String],
+                       fs: FileSystem[ID, FILE, FOLDER, Model.FolderContent[ID, FILE, FOLDER]],
+                       cryptConfig: CryptConfig)
+                      (implicit system: ActorSystem[_]): Operation[ID] = {
+    implicit val ec: ExecutionContext = system.executionContext
+    val root = fs.rootID
+    if (components.isEmpty) root
+    else root flatMap (resolveNextPathComponent(_, components, fs, cryptConfig))
+  }
+
+  /**
+   * Tries to resolve a path component in the content of the folder with the
+   * given ID. This function is invoked recursively until all path components
+   * have been resolved or an unresolvable component is encountered. In the
+   * latter case, the resulting ''Future'' fails with an
+   * ''IllegalArgumentException'' exception whose message lists the components
+   * that could not be resolved.
+   *
+   * @param folderID   the ID of the current folder
+   * @param components the remaining components to be resolved
+   * @param fs         the file system
+   * @param config     the cryptography-related configuration
+   * @param system     the actor system
+   * @return an ''Operation'' with the ID of the resolved element
+   */
+  private def resolveNextPathComponent(folderID: ID, components: Seq[String],
+                                       fs: FileSystem[ID, FILE, FOLDER, Model.FolderContent[ID, FILE, FOLDER]],
+                                       config: CryptConfig)
+                                      (implicit system: ActorSystem[_]): Operation[ID] = {
+    implicit val ec: ExecutionContext = system.executionContext
+    fs.folderContent(folderID) flatMap { content =>
+      val optId = findPathComponent(content.folders, components.head, config)
+        .orElse(if (components.size == 1) findPathComponent(content.files, components.head, config) else None)
+      optId.fold(failedResolveOperation(components)) { id =>
+        if (components.size == 1) Operation(_ => Future.successful(id))
+        else resolveNextPathComponent(id, components.tail, fs, config)
+      }
+    }
+  }
+
+  /**
+   * Returns an ''Operation'' to report a failure when resolving path
+   * components.
+   *
+   * @param components the components that could not be resolved
+   * @return the ''Operation'' producing a failed ''Future''
+   */
+  private def failedResolveOperation(components: Seq[String]): Operation[ID] = Operation { _ =>
+    Future.failed(
+      new IllegalArgumentException("Could not resolve all path components. Remaining components: " + components))
+  }
+
+  /**
+   * Looks up an element name in an encrypted map with folder content elements.
+   *
+   * @param elements the map with encrypted elements
+   * @param name     the name to be searched for
+   * @param config   the cryptography-related configuration
+   * @tparam A the element type of the map
+   * @return an ''Option'' with the ID of the element that was found
+   */
+  private def findPathComponent[A <: Model.Element[ID]](elements: Map[ID, A], name: String, config: CryptConfig):
+  Option[ID] =
+    elements.find(e => decryptElementName(e._2, config) == name) map (_._1)
+
+  /**
+   * Returns the decrypted name of the passed in element.
+   *
+   * @param elem   the element
+   * @param config the cryptography-related configuration
+   * @return the decrypted name of this element
+   */
+  private def decryptElementName(elem: Model.Element[ID], config: CryptConfig): String =
+    CryptService.decryptTextFromBase64(config.algorithm, config.keyDecrypt, elem.name)(config.secRandom)
+}
---
+++ crypt/src/test/scala/com/github/cloudfiles/crypt/fs/resolver/PathComponentsResolverSpec.scala
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.crypt.fs.resolver
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import com.github.cloudfiles.core.{FileSystem, Model}
+import org.mockito.Mockito.when
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+/**
+ * Test class for ''PathComponentResolver''.
+ */
+class PathComponentsResolverSpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers
+  with MockitoSugar {
+
+  import com.github.cloudfiles.crypt.fs.CryptFileSystemTestHelper._
+
+  /**
+   * Creates a mock for a file system, which can be passed to a test resolver
+   * instance.
+   *
+   * @return the mock file system
+   */
+  private def createFileSystemMock(): FileSystem[String, FileType, FolderType,
+    Model.FolderContent[String, FileType, FolderType]] =
+    mock[FileSystem[String, FileType, FolderType, Model.FolderContent[String, FileType, FolderType]]]
+
+  /**
+   * Creates a mock for a file with the given properties.
+   *
+   * @param idx  the index of the test file
+   * @param name the name of this file
+   * @return the mock for this test file
+   */
+  private def createFileMock(idx: Int, name: String): FileType =
+    initMock(mock[FileType], fileID(idx), name)
+
+  /**
+   * Creates a mock for a folder with the given properties.
+   *
+   * @param idx  the index of the test folder
+   * @param name the name of this folder
+   * @return the mock for this test folder
+   */
+  private def createFolderMock(idx: Int, name: String): FolderType =
+    initMock(mock[FolderType], folderID(idx), name)
+
+  /**
+   * Creates a test resolver instance.
+   *
+   * @return the test resolver
+   */
+  private def createResolver(): PathComponentsResolver[String, FileType, FolderType] =
+    new PathComponentsResolver[String, FileType, FolderType]
+
+  "PathComponentResolver" should "resolve an empty path to the root folder ID" in {
+    val fs = createFileSystemMock()
+    when(fs.rootID).thenReturn(stubOperation(FileID))
+    val resolver = createResolver()
+
+    runOp(testKit, resolver.resolve(Nil, fs, DefaultCryptConfig)) should be(FileID)
+  }
+
+  it should "resolve a path to an existing folder" in {
+    val RootID = "theRootFolder"
+    val components = Seq("the", "desired", "folder")
+    val rootFolders = Map(folderID(1) -> createFolderMock(1, encryptName(folderName(1))),
+      folderID(2) -> createFolderMock(2, encryptName(folderName(2))),
+      folderID(3) -> createFolderMock(3, encryptName("the")))
+    val rootContent = Model.FolderContent(RootID, Map.empty[String, FileType], rootFolders)
+    val level1Folders = Map(folderID(4) -> createFolderMock(4, encryptName("desired")),
+      folderID(5) -> createFolderMock(5, encryptName(folderName(5))))
+    val level1Files = Map(fileID(1) -> createFileMock(1, encryptName(fileName(1))))
+    val level1Content = Model.FolderContent(folderID(3), level1Files, level1Folders)
+    val level2Folders = Map(folderID(6) -> createFolderMock(6, encryptName(folderName(6))),
+      folderID(7) -> createFolderMock(7, encryptName(folderName(7))),
+      folderID(8) -> createFolderMock(8, encryptName("folder")),
+      folderID(9) -> createFolderMock(9, encryptName(folderName(9))))
+    val level2Files = Map(fileID(2) -> createFileMock(2, encryptName(fileName(2))))
+    val level2Content = Model.FolderContent(folderID(4), level2Files, level2Folders)
+    val fs = createFileSystemMock()
+    when(fs.rootID).thenReturn(stubOperation(RootID))
+    when(fs.folderContent(RootID)).thenReturn(stubOperation(rootContent))
+    when(fs.folderContent(level1Content.folderID)).thenReturn(stubOperation(level1Content))
+    when(fs.folderContent(level2Content.folderID)).thenReturn(stubOperation(level2Content))
+    val resolver = createResolver()
+
+    runOp(testKit, resolver.resolve(components, fs, DefaultCryptConfig)) should be(folderID(8))
+  }
+
+  it should "resolve a path to an existing file" in {
+    val RootID = "theRootFolder"
+    val components = Seq("the", "desired", "test file.doc")
+    val rootFolders = Map(folderID(1) -> createFolderMock(1, encryptName(folderName(1))),
+      folderID(2) -> createFolderMock(2, encryptName(folderName(2))),
+      folderID(3) -> createFolderMock(3, encryptName("the")))
+    val rootContent = Model.FolderContent(RootID, Map.empty[String, FileType], rootFolders)
+    val level1Folders = Map(folderID(4) -> createFolderMock(4, encryptName("desired")),
+      folderID(5) -> createFolderMock(5, encryptName(folderName(5))))
+    val level1Files = Map(fileID(1) -> createFileMock(1, encryptName(fileName(1))))
+    val level1Content = Model.FolderContent(folderID(3), level1Files, level1Folders)
+    val level2Folders = Map(folderID(6) -> createFolderMock(6, encryptName(folderName(6))))
+    val level2Files = Map(fileID(2) -> createFileMock(2, encryptName(fileName(2))),
+      fileID(3) -> createFileMock(3, encryptName("test file.doc")),
+      fileID(4) -> createFileMock(4, encryptName(fileName(4))))
+    val level2Content = Model.FolderContent(folderID(4), level2Files, level2Folders)
+    val fs = createFileSystemMock()
+    when(fs.rootID).thenReturn(stubOperation(RootID))
+    when(fs.folderContent(RootID)).thenReturn(stubOperation(rootContent))
+    when(fs.folderContent(level1Content.folderID)).thenReturn(stubOperation(level1Content))
+    when(fs.folderContent(level2Content.folderID)).thenReturn(stubOperation(level2Content))
+    val resolver = createResolver()
+
+    runOp(testKit, resolver.resolve(components, fs, DefaultCryptConfig)) should be(fileID(3))
+  }
+
+  it should "handle a path that cannot be resolved" in {
+    val RootID = "theRootFolder"
+    val components = Seq("the", "desired", "folder")
+    val rootFolders = Map(folderID(1) -> createFolderMock(1, encryptName(folderName(1))),
+      folderID(2) -> createFolderMock(2, encryptName(folderName(2))),
+      folderID(3) -> createFolderMock(3, encryptName("the")))
+    val rootFiles = Map(fileID(0) -> createFileMock(0, encryptName("the")))
+    val rootContent = Model.FolderContent(RootID, rootFiles, rootFolders)
+    val level1Folders = Map(folderID(4) -> createFolderMock(4, encryptName(folderName(4))),
+      folderID(5) -> createFolderMock(5, encryptName(folderName(5))))
+    val level1Files = Map(fileID(1) -> createFileMock(1, encryptName(fileName(1))),
+      fileID(2) -> createFileMock(2, encryptName("desired")))
+    val level1Content = Model.FolderContent(folderID(3), level1Files, level1Folders)
+    val fs = createFileSystemMock()
+    when(fs.rootID).thenReturn(stubOperation(RootID))
+    when(fs.folderContent(RootID)).thenReturn(stubOperation(rootContent))
+    when(fs.folderContent(level1Content.folderID)).thenReturn(stubOperation(level1Content))
+    val resolver = createResolver()
+
+    val ex = expectFailedFuture[IllegalArgumentException](runOpFuture(testKit,
+      resolver.resolve(components, fs, DefaultCryptConfig)))
+    ex.getMessage should include(components.tail.toString())
+  }
+}

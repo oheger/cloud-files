67951c62356021c0b6b962b85d820dd3878cb783
oheger <oliver.heger@oliver-heger.de>
2021-05-21 20:56:00 +0200

    Added a configuration class for local file systems.
    The class defines some properties that control the exact behavior of
    the local file system.

---
+++ localfs/src/main/scala/com/github/cloudfiles/localfs/LocalFsConfig.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.localfs
+
+import java.nio.file.{LinkOption, Path}
+import scala.concurrent.ExecutionContext
+
+/**
+ * The configuration class for the local file system implementation.
+ *
+ * The most important property is the base path of the local file system.
+ * Relative paths are resolved against this root folder.
+ *
+ * As the file system implementation typically uses blocking operations, a
+ * dedicated ''ExecutionContext'' has to be provided. This is to force the user
+ * to think about this option, because simply using the execution context of an
+ * already available actor system may not be the best option.
+ *
+ * @param basePath         the base path of the file system; this should be an
+ *                         absolute path
+ * @param executionContext the execution context
+ * @param linkOptions      options for dealing with links
+ */
+case class LocalFsConfig(basePath: Path,
+                         executionContext: ExecutionContext,
+                         linkOptions: Seq[LinkOption] = Nil)

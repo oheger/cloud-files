b94646b2c0e7491fda49b15e937f0a64bf597aab
oheger <oliver.heger@oliver-heger.de>
2021-08-15 22:05:20 +0200

    Added a configuration class for the GoogleDrive implementation.
    This class allows fine-tuning the interaction with the GoogleDrive API
    server.

---
+++ gdrive/src/main/scala/com/github/cloudfiles/gdrive/GoogleDriveConfig.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.cloudfiles.gdrive
+
+import akka.util.Timeout
+
+import scala.concurrent.duration.DurationInt
+
+object GoogleDriveConfig {
+  /** The default root URI for the GoogleDrive API. */
+  final val GoogleDriveServerUri = "https://www.googleapis.com"
+
+  /**
+   * The default timeout for requests against the GoogleDrive server. This value
+   * is used if the user does not provide an explicit timeout.
+   */
+  final val DefaultTimeout = Timeout(30.seconds)
+}
+
+/**
+ * A configuration class for the GoogleDrive ''FileSystem'' implementation.
+ *
+ * Using this class, certain aspects of the behavior of the GoogleDrive
+ * implementation can be configured. Configuration is optional, meaningful
+ * default values are set for all of the properties.
+ *
+ * @param serverUri   the root URI of the GoogleDrive API
+ * @param optRootPath an optional root path; if defined, all paths are resolved
+ *                    relatively to this root path
+ * @param timeout     the timeout for server requests
+ */
+case class GoogleDriveConfig(serverUri: String = GoogleDriveConfig.GoogleDriveServerUri,
+                             optRootPath: Option[String] = None,
+                             timeout: Timeout = GoogleDriveConfig.DefaultTimeout)
